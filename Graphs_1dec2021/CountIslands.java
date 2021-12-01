d589528da478430637c41aae70ae440b77e4197e
Amit Anand <dropmail.amit@gmail.com>
2021-12-01 13:41:36 +0530

    updated.

--- Graphs_1dec2021/CountIslands.java
+++ Graphs_1dec2021/CountIslands.java
@@ -1,8 +1,6 @@
 package Graphs_1dec2021;
 
 import java.io.*;
-import java.util.*;
-
 public class CountIslands {
    public static void main(String[] args) throws Exception {
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
