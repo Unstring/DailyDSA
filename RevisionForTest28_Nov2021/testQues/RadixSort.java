970e869236684fcaf8571c169d816afd37692e54
Amit Anand <dropmail.amit@gmail.com>
2021-12-01 12:10:12 +0530

    accepted changes in radix sort.

--- RevisionForTest28_Nov2021/testQues/RadixSort.java
+++ RevisionForTest28_Nov2021/testQues/RadixSort.java
@@ -3,7 +3,6 @@ package RevisionForTest28_Nov2021.testQues;
 import java.util.*;
 
 public class RadixSort {
-
   public static void radixSort(int[] arr) {
     // write code here
     int max = Integer.MIN_VALUE;
@@ -14,7 +13,6 @@ public class RadixSort {
       countSort(arr, exp);
     }
   }
-
   public static void countSort(int[] arr, int exp) {
     // write code here
     int[] ans = new int[arr.length];
@@ -22,17 +20,14 @@ public class RadixSort {
     for(int i = 0; i < arr.length; i++){
         farr[(arr[i]/exp)%10]++;
     }
-    
     for(int i = 1; i < farr.length; i++){
         farr[i] += farr[i -1];
     }
-
     for(int i = arr.length - 1; i >= 0 ; i--){
         int pos = farr[(arr[i]/exp)%10] - 1;
         ans[pos] = arr[i];
         farr[(arr[i]/exp)%10]--;
     }
-
     for(int i = 0; i < arr.length; i++){
         arr[i] = ans[i];
     }
@@ -40,14 +35,12 @@ public class RadixSort {
     System.out.print("After sorting on " + exp + " place -> ");
     print(arr);
   }
-
   public static void print(int[] arr) {
     for (int i = 0; i < arr.length; i++) {
       System.out.print(arr[i] + " ");
     }
     System.out.println();
   }
-
   public static void main(String[] args) throws Exception {
     Scanner scn = new Scanner(System.in);
     int n = scn.nextInt();
@@ -59,5 +52,4 @@ public class RadixSort {
     print(arr);
     scn.close();
   }
-
 }
