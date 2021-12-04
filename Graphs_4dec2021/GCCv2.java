bc1e0b051ba0ad5a454c3990f091cbd71ee86425
Amit Anand <dropmail.amit@gmail.com>
2021-12-04 23:42:26 +0530

    gcc v2 complete.

--- Graphs_4dec2021/GCCv2.java
+++ Graphs_4dec2021/GCCv2.java
@@ -4,42 +4,67 @@ import java.io.*;
 import java.util.*;
 
 public class GCCv2 {
-   static class Edge {
-      int src;
-      int nbr;
-      int wt;
+    static class Edge {
+        int src;
+        int nbr;
+        int wt;
 
-      Edge(int src, int nbr, int wt) {
-         this.src = src;
-         this.nbr = nbr;
-         this.wt = wt;
-      }
-   }
+        Edge(int src, int nbr, int wt) {
+            this.src = src;
+            this.nbr = nbr;
+            this.wt = wt;
+        }
+    }
 
-   public static void main(String[] args) throws Exception {
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void getSingleConnectedComp(int src, ArrayList<Edge>[]graph, ArrayList<Integer>scc, boolean[]vis){
+        scc.add(src);
+        vis[src] = true;
 
-      int vtces = Integer.parseInt(br.readLine());
-      ArrayList<Edge>[] graph = new ArrayList[vtces];
-      for (int i = 0; i < vtces; i++) {
-         graph[i] = new ArrayList<>();
-      }
+        for(Edge edge : graph[src]){
+            int nbr = edge.nbr;
 
-      int edges = Integer.parseInt(br.readLine());
-      for (int i = 0; i < edges; i++) {
-         String[] parts = br.readLine().split(" ");
-         int v1 = Integer.parseInt(parts[0]);
-         int v2 = Integer.parseInt(parts[1]);
-         int wt = Integer.parseInt(parts[2]);
-         graph[v1].add(new Edge(v1, v2, wt));
-         graph[v2].add(new Edge(v2, v1, wt));
-      }
+            if(vis[nbr] == false){
+                getSingleConnectedComp(nbr, graph, scc, vis);
+            }
+        }
+    }
 
-      ArrayList<ArrayList<Integer>> comps = new ArrayList<>();
-      
-      // write your code here
-      
+    public static void GetConnectedCompoents(ArrayList<Edge>[] graph, ArrayList<ArrayList<Integer>> comps) {
+        boolean[] vis = new boolean[graph.length];
 
-      System.out.println(comps);
-   }
+        for(int i = 0; i < graph.length; i++){
+            if(vis[i] == false){
+                ArrayList<Integer> scc = new ArrayList<>();
+                getSingleConnectedComp(i, graph, scc, vis);
+                comps.add(scc);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int vtces = Integer.parseInt(br.readLine());
+        ArrayList<Edge>[] graph = new ArrayList[vtces];
+        for (int i = 0; i < vtces; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        int edges = Integer.parseInt(br.readLine());
+        for (int i = 0; i < edges; i++) {
+            String[] parts = br.readLine().split(" ");
+            int v1 = Integer.parseInt(parts[0]);
+            int v2 = Integer.parseInt(parts[1]);
+            int wt = Integer.parseInt(parts[2]);
+            graph[v1].add(new Edge(v1, v2, wt));
+            graph[v2].add(new Edge(v2, v1, wt));
+        }
+
+        ArrayList<ArrayList<Integer>> comps = new ArrayList<>();
+
+        // write your code here
+        GetConnectedCompoents(graph, comps);
+
+        System.out.println(comps);
+    }
 }
